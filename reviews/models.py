655fe1c6f11997a209e6f09c337f9f58280e48e2
koffcheg <koffcheg@koffchegs-Laptop.local>
2023-08-19 15:24:32 -0700

    migration

--- reviews/models.py
+++ reviews/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publisher = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.title
@@ -12,7 +12,6 @@ class Review(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     review_text = models.TextField()
     rating = models.PositiveIntegerField()
-
 
     def __str__(self):
         return f"Review for {self.book.title}"
