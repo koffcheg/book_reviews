672e542cae73d771552dd43ef50af1f84b63f1ad
koffcheg <koffcheg@koffchegs-Laptop.local>
2023-08-19 14:52:42 -0700

    update db

--- reviews/models.py
+++ reviews/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-
+    publisher = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
