a66e1fc3044273348314b50974d391c65a25e382
koffcheg <koffcheg@koffchegs-Laptop.local>
2023-08-20 08:58:05 -0700

    update with tests

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Book, Review
 
-# Create your tests here.
+# Model Tests
+class BookModelTest(TestCase):
+    def setUp(self):
+        Book.objects.create(title='Test Book', 
+                            author='Test Author', 
+                            publisher = 'Test Publiher',
+                            release_date = '2028-08-01',)
+
+    def test_book_creation(self):
+        book = Book.objects.get(title='Test Book')
+        self.assertEqual(book.author, 'Test Author')
+        self.assertEqual(book.publisher, 'Test Publiher')
+        self.assertEqual(book.release_date, '2028-08-01')
+
+# View Tests
+class BookListViewTest(TestCase):
+    def setUp(self):
+        Book.objects.create(title='Test Book', 
+                            author='Test Author', 
+                            publisher = 'Test Publiher',
+                            release_date = '2028-08-01',)
+
+    def test_view_url_exists(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'reviews/book_list.html')
