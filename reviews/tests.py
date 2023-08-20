4f3b3c4952ef68a4d88ce72edea945d2df072236
koffcheg <koffcheg@koffchegs-Laptop.local>
2023-08-20 09:21:08 -0700

    update database

--- reviews/tests.py
+++ reviews/tests.py
@@ -9,21 +9,24 @@ class BookModelTest(TestCase):
         Book.objects.create(title='Test Book', 
                             author='Test Author', 
                             publisher='Test Publisher',
-                            release_date='2028-08-01')
+                            release_date='2028-08-01',
+                            amount=0)
 
     def test_book_creation(self):
         book = Book.objects.get(title='Test Book')
         self.assertEqual(book.author, 'Test Author')
         self.assertEqual(book.publisher, 'Test Publisher')
-        self.assertEqual(book.release_date, date(2028, 8, 1))  
-        
+        self.assertEqual(book.release_date, date(2028, 8, 1))
+        self.assertEqual(book.amount, 0)  
+
 # View Tests
 class BookListViewTest(TestCase):
     def setUp(self):
         Book.objects.create(title='Test Book', 
                             author='Test Author', 
                             publisher='Test Publisher',
-                            release_date='2028-08-01')
+                            release_date='2028-08-01',
+                            amount=0)
 
     def test_view_url_exists(self):
         response = self.client.get(reverse('book_list'))
