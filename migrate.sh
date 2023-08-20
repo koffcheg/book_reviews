89d0a6268abc6a64aa10b26878d2794e025b66ab
koffcheg <koffcheg@koffchegs-Laptop.local>
2023-08-20 13:39:58 -0700

    update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -3,13 +3,14 @@
 git fetch origin main
 
 # Check for migration conflicts by comparing local and remote branches
-git diff HEAD..origin/main --name-only -- '*/migrations/*.py' | grep '*/migrations/*.py'
+CONFLICT_FILES=$(git diff HEAD..origin/main --name-only -- '*/migrations/*.py')
 
 # If diff command returns non-empty output, there are migration conflicts
-if [ $? -eq 0 ]; then
+if [ -z "$CONFLICT_FILES" ]; then
   echo 'No migration conflicts detected.'
 else
-  echo 'Migration conflict detected. Exiting.'
+  echo 'Migration conflict detected in the following files:'
+  echo "$CONFLICT_FILES"
   exit 1
 fi
 
